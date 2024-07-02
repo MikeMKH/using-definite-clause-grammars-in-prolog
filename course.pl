177e8919bf36118b4cbbc3be55e2052b4932590d
Mike Harris <MikeMKH@users.noreply.github.com>
2024-07-02 06:19:37 -0500

    add Relating Trees To Lists

--- course.pl
+++ course.pl
@@ -19,6 +19,51 @@ test(pharse_xXx_X_must_be_x) :-
   phrase(xs, [x,X,x]),
   assertion(X == x).
 
+in_tree_nodes(nil) --> [].
+in_tree_nodes(node(Name, Left, Right)) -->
+  in_tree_nodes(Left),
+  [Name],
+  in_tree_nodes(Right).
+
+test(parse_in_tree_nodes_abcd) :-
+  assertion(phrase(
+    in_tree_nodes(node(
+      a,
+      node(b, nil,
+        node(c, nil, nil)),
+      node(d, nil, nil))),
+    [b, c, a, d])).
+
+pre_tree_nodes(nil) --> [].
+pre_tree_nodes(node(Name, Left, Right)) -->
+  [Name],
+  pre_tree_nodes(Left),
+  pre_tree_nodes(Right).
+
+test(parse_pre_tree_nodes_abcd) :-
+  assertion(phrase(
+    pre_tree_nodes(node(
+      a,
+      node(b, nil,
+        node(c, nil, nil)),
+      node(d, nil, nil))),
+    [a, b, c, d])).
+
+post_tree_nodes(nil) --> [].
+post_tree_nodes(node(Name, Left, Right)) -->
+  post_tree_nodes(Left),
+  post_tree_nodes(Right),
+  [Name].
+
+test(parse_post_tree_nodes_abcd) :-
+  assertion(phrase(
+    post_tree_nodes(node(
+      a,
+      node(b, nil,
+        node(c, nil, nil)),
+      node(d, nil, nil))),
+    [c, b, d, a])).
+
 :- end_tests(course).
 :- run_tests.
 
