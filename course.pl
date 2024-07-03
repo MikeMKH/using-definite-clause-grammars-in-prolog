924ea51da774553cd554f9fa726669c26ccce5d6
Mike Harris <MikeMKH@users.noreply.github.com>
2024-07-03 06:07:28 -0500

    add Left Recursion

--- course.pl
+++ course.pl
@@ -100,3 +100,99 @@ mns1 --> [n], mns.
 % Ls = [m, n] ;
 % Ls = [m, n, m, n] ;
 % Ls = [m, n, m, n, m, n] .
+
+tree_nodes(nil) --> [].
+tree_nodes(node(Name, Left, Right)) -->
+  tree_nodes(Left),
+  [Name],
+  tree_nodes(Right).
+
+% ?- phrase(tree_nodes(Tree), [a,b,c,d]).
+% Tree = node(a, nil, node(b, nil, node(c, nil, node(d, nil, nil)))) ;
+% ERROR: Stack limit (1.0Gb) exceeded
+% ERROR:   Stack sizes: local: 0.7Gb, global: 0.2Gb, trail: 0Kb
+% ERROR:   Stack depth: 6,145,592, last-call: 0%, Choice points: 7
+% ERROR:   Possible non-terminating recursion:
+% ERROR:     [6,145,592] user:tree_nodes(_61472678, [], _61472682)
+% ERROR:     [6,145,591] user:tree_nodes(<compound node/3>, [], _61472704)
+
+tree_nodes(nil, Ls, Ls) --> [].
+tree_nodes(node(Name, Left, Right), [_|Ls0], Ls) -->
+  tree_nodes(Left, Ls0, Ls1),
+  [Name],
+  tree_nodes(Right, Ls1, Ls).
+
+% ?- Ns = [a,b,c,d], phrase(tree_nodes(Tree, Ns, _), Ns).
+% Ns = [a, b, c, d],
+% Tree = node(a, nil, node(b, nil, node(c, nil, node(d, nil, nil)))) ;
+% Ns = [a, b, c, d],
+% Tree = node(a, nil, node(b, nil, node(d, node(c, nil, nil), nil))) ;
+% Ns = [a, b, c, d],
+% Tree = node(a, nil, node(c, node(b, nil, nil), node(d, nil, nil))) ;
+% Ns = [a, b, c, d],
+% Tree = node(a, nil, node(d, node(b, nil, node(c, nil, nil)), nil)) ;
+% Ns = [a, b, c, d],
+% Tree = node(a, nil, node(d, node(c, node(b, nil, nil), nil), nil)) ;
+% Ns = [a, b, c, d],
+% Tree = node(b, node(a, nil, nil), node(c, nil, node(d, nil, nil))) .
+
+% ?- Ns = [a,b,c,d], phrase(tree_nodes(Tree, _, Ns), Ns).
+% Ns = [a, b, c, d],
+% Tree = node(a, nil, node(b, nil, node(c, nil, node(d, nil, nil)))) ;
+% ERROR: Stack limit (1.0Gb) exceeded
+% ERROR:   Stack sizes: local: 0.5Gb, global: 0.2Gb, trail: 0Kb
+% ERROR:   Stack depth: 3,355,311, last-call: 0%, Choice points: 7
+% ERROR:   In:
+% ERROR:     [3,355,311] user:tree_nodes(_60394386, _60394388, _60394390, [], _60394394)
+% ERROR:     [3,355,310] user:tree_nodes(<compound node/3>, [length:1|_60394432], _60394416, [], _60394420)
+% ERROR:     [3,355,309] user:tree_nodes(<compound node/3>, [length:2|_60394470], _60394454, [], _60394458)
+% ERROR:     [3,355,308] user:tree_nodes(<compound node/3>, [length:3|_60394508], _60394492, [], _60394496)
+% ERROR:     [3,355,307] user:tree_nodes(<compound node/3>, [length:4|_60394546], _60394530, [], _60394534)
+% ERROR: 
+% ERROR: Use the --stack_limit=size[KMG] command line option or
+% ERROR: ?- set_prolog_flag(stack_limit, 2_147_483_648). to double the limit.
+
+% [trace]  ?- Ns = [a,b,c], phrase(tree_nodes(Tree, Ns, _), Ns).
+%    Call: (11) _7666=[a, b, c] ? creep
+%    Exit: (11) [a, b, c]=[a, b, c] ? creep
+% ^  Call: (11) phrase(tree_nodes(_7672, [a, b, c], _7676), [a, b, c]) ? creep
+%    Call: (14) tree_nodes(_7672, [a, b, c], _7676, [a, b, c], []) ? creep
+%    Call: (15) [a, b, c]=[] ? creep
+%    Fail: (15) [a, b, c]=[] ? creep
+%    Redo: (14) tree_nodes(_7672, [a, b, c], _7676, [a, b, c], []) ? creep
+%    Call: (15) tree_nodes(_14168, [b, c], _14172, [a, b, c], _14174) ? creep
+%    Call: (16) [a, b, c]=_14174 ? creep
+%    Exit: (16) [a, b, c]=[a, b, c] ? creep
+%    Exit: (15) tree_nodes(nil, [b, c], [b, c], [a, b, c], [a, b, c]) ? creep
+%    Call: (15) [a, b, c]=[_14166|_17444] ? creep
+%    Exit: (15) [a, b, c]=[a, b, c] ? creep
+%    Call: (15) tree_nodes(_14170, [b, c], _7676, [b, c], []) ? creep
+%    Call: (16) [b, c]=[] ? creep
+%    Fail: (16) [b, c]=[] ? creep
+%    Redo: (15) tree_nodes(_14170, [b, c], _7676, [b, c], []) ? creep
+%    Call: (16) tree_nodes(_22334, [c], _22338, [b, c], _22340) ? creep
+%    Call: (17) [b, c]=_22340 ? creep
+%    Exit: (17) [b, c]=[b, c] ? creep
+%    Exit: (16) tree_nodes(nil, [c], [c], [b, c], [b, c]) ? creep
+%    Call: (16) [b, c]=[_22332|_25610] ? creep
+%    Exit: (16) [b, c]=[b, c] ? creep
+%    Call: (16) tree_nodes(_22336, [c], _7676, [c], []) ? creep
+%    Call: (17) [c]=[] ? creep
+%    Fail: (17) [c]=[] ? creep
+%    Redo: (16) tree_nodes(_22336, [c], _7676, [c], []) ? creep
+%    Call: (17) tree_nodes(_30500, [], _30504, [c], _30506) ? creep
+%    Call: (18) [c]=_30506 ? creep
+%    Exit: (18) [c]=[c] ? creep
+%    Exit: (17) tree_nodes(nil, [], [], [c], [c]) ? creep
+%    Call: (17) [c]=[_166|_1540] ? creep
+%    Exit: (17) [c]=[c] ? creep
+%    Call: (17) tree_nodes(_170, [], _22, [], []) ? creep
+%    Call: (18) []=[] ? creep
+%    Exit: (18) []=[] ? creep
+%    Exit: (17) tree_nodes(nil, [], [], [], []) ? creep
+%    Exit: (16) tree_nodes(node(c, nil, nil), [c], [], [c], []) ? creep
+%    Exit: (15) tree_nodes(node(b, nil, node(c, nil, nil)), [b, c], [], [b, c], []) ? creep
+%    Exit: (14) tree_nodes(node(a, nil, node(b, nil, node(c, nil, nil))), [a, b, c], [], [a, b, c], []) ? creep
+% ^  Exit: (11) phrase(user:tree_nodes(node(a, nil, node(b, nil, node(c, nil, nil))), [a, b, c], []), [a, b, c]) ? creep
+% Ns = [a, b, c],
+% Tree = node(a, nil, node(b, nil, node(c, nil, nil))) .
